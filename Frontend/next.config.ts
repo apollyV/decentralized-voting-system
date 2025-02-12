7c6e070f6655cc8db5c8ff5897167fdd4d7ba442
Constant Vennin <constantvenninp@gmail.com>
2025-02-12 16:16:20 +0100

    resolve build bug

--- Frontend/next.config.ts
+++ Frontend/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    console.log("Webpack Configuration", { isServer });
     config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
   },
